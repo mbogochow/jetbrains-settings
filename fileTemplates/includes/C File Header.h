8169d5346e07575d0c10f527cef81ff50b350516
Mike Bogochow <mbogochow@hotmail.com>
2018-03-14 16:59:14 -0400

    CL-2017.3.4 <mbogochow@mbogobuntu Update baseRefactoring.xml, ide.general.xml, project.default.xml, diff.xml, user.xml, Default.xml, file.template.settings.xml, keymap.xml, find.xml, ui.lnf.xml, filetypes.xml, code.style.schemes.xml, colors.scheme.xml, Darcula copy.icls, editor.xml, usageView.xml, debugger.xml	Create Traditional.xml, Default _1_.xml, Mine.xml, Kevin C__.xml, Default copy.xml, other.xml, C_C__.xml, C File Header.h, CMake.xml

---
+++ fileTemplates/includes/C File Header.h
@@ -0,0 +1,20 @@
+#if ($HEADER_COMMENTS)
+/*******************************************************************************
+* @file $FILE_NAME
+*
+* Copyright (c) $YEAR Software Forge Inc.
+*
+*
+* Proprietary and Confidential
+* Unauthorized distribution or copying is prohibited
+* All rights reserved.
+*
+* No part of this computer software may be reprinted, reproduced or
+* utilized in any form or by any electronic, mechanical, or other means,
+* now known or hereafter invented, including photocopying and recording, or
+* using any information storage and retrieval system, without permission in
+* writing from Software Forge Inc.
+*
+*******************************************************************************/
+#end
+
